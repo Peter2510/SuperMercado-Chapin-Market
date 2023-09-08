3c65b6ed024c5d4d99635fe8146a145a8973455a
Peter2510 <pedrogordillo202031683@cunoc.edu.gt>
2023-09-07 22:22:23 -0600

    Agregando backend

---
+++ api-chapin-market/src/main/java/com/peter/apichapinmarket/ApiChapinMarketApplication.java
@@ -0,0 +1,13 @@
+package com.peter.apichapinmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiChapinMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiChapinMarketApplication.class, args);
+	}
+
+}
---
+++ api-chapin-market/src/test/java/com/peter/apichapinmarket/ApiChapinMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peter.apichapinmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiChapinMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
