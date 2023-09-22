0808d0c95264685d8eebe42eacba8d6495285274
Peter2510 <pedrogordillo202031683@cunoc.edu.gt>
2023-09-22 12:47:22 -0600

    Agregando dashboard administrador

--- chapin_market/src/main/java/com/peter/chapin_market/controlador/EmpleadoControlador.java
+++ chapin_market/src/main/java/com/peter/chapin_market/controlador/EmpleadoControlador.java
@@ -21,64 +21,66 @@ import com.peter.chapin_market.modelo.Empleado;
 public class EmpleadoControlador {
 
 	@Autowired
-    private DataSource dataSource; // Inyecta el DataSource "configuracion de la base de datos"
-	
+	private DataSource dataSource; // Inyecta el DataSource "configuracion de la base de datos"
+
 	@GetMapping(value = "/chapinMarket/empleados", produces = "application/json")
 	public ResponseEntity<List<Empleado>> obtenerTodosLosEmpleados() {
-		
-        List<Empleado> empleados = new ArrayList<Empleado>();
-        
-        try {
-        	
-        	Connection connection = dataSource.getConnection();
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM empleados.empleado");
 
-            while (resultSet.next()) {
-                Empleado empleado = new Empleado();
-                empleado.setCodigo(resultSet.getInt("codigo"));
-                empleado.setNombre(resultSet.getString("nombre"));
-                empleado.setCodigo_sucursal(resultSet.getInt("codigo_sucursal"));
-                switch (empleado.getCodigo_sucursal()) {
-				case 1: 
-				empleado.setNombre_sucursal("Central");
-				break;
-				case 2: 
-				empleado.setNombre_sucursal("Norte");
-				break;
-				case 3: 
-				empleado.setNombre_sucursal("Sur");
-				break;
-				}                    
-                empleado.setCodigo_rol(resultSet.getInt("rol"));
-                switch (empleado.getCodigo_rol()) {
-				case 1: 
-				empleado.setNombre_rol("Cajero");
-				break;
-				case 2: 
-				empleado.setNombre_rol("Bodega");
-				break;
-				case 3: 
-				empleado.setNombre_rol("Inventario");
-				break;
-				case 4: 
-				empleado.setNombre_rol("Administrador");
-				break;
+		List<Empleado> empleados = new ArrayList<Empleado>();
+
+		try {
+
+			Connection connection = dataSource.getConnection();
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT codigo,nombre,codigo_sucursal,rol FROM empleados.empleado");
+
+			while (resultSet.next()) {
+				Empleado empleado = new Empleado();
+				empleado.setCodigo(resultSet.getInt("codigo"));
+				empleado.setNombre(resultSet.getString("nombre"));
+				empleado.setCodigo_sucursal(resultSet.getInt("codigo_sucursal"));
+				switch (empleado.getCodigo_sucursal()) {
+				case 1:
+					empleado.setNombre_sucursal("Central");
+					break;
+				case 2:
+					empleado.setNombre_sucursal("Norte");
+					break;
+				case 3:
+					empleado.setNombre_sucursal("Sur");
+					break;
 				}
-                
-                
+				empleado.setCodigo_rol(resultSet.getInt("rol"));
+				switch (empleado.getCodigo_rol()) {
+				case 1:
+					empleado.setNombre_rol("Cajero");
+					break;
+				case 2:
+					empleado.setNombre_rol("Bodega");
+					break;
+				case 3:
+					empleado.setNombre_rol("Inventario");
+					break;
+				case 4:
+					empleado.setNombre_rol("Administrador");
+					break;
+				}
 
+				empleados.add(empleado);
+			}
 
-                empleados.add(empleado);
-            }
-        
-            return ResponseEntity.ok(empleados);
-            
-        } catch (SQLException e) {
-            // Manejo de excepciones
-        	return ResponseEntity.badRequest().build();
-        }
-        
-    }
-	
+			resultSet.close();
+			statement.close();
+			connection.close();
+
+			return ResponseEntity.ok(empleados);
+
+		} catch (SQLException e) {
+			// Manejo de excepciones
+			System.out.print(e.getMessage());
+			return ResponseEntity.badRequest().build();
+
+		}
+	}
+
 }
--- fronted-chapin-market/src/app/app.module.ts
+++ fronted-chapin-market/src/app/app.module.ts
@@ -10,7 +10,19 @@ import { VentaComponent } from './cajero/venta/venta.component';
 import { InventarioComponent } from './sucursal/inventario/inventario/inventario.component';
 import { BodegaComponent } from './sucursal/bodega/bodega/bodega.component';
 import { ReporteVentasComponent } from './administrador/reporteVentas/reporte-ventas/reporte-ventas.component';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './administrador/dashboard/dashboard.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { NavegacionComponent } from './administrador/navegacion/navegacion.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu'
 
 const routes: Route[] = [
   { path: "", redirectTo:'login',pathMatch:'full' },
@@ -32,13 +44,25 @@ const routes: Route[] = [
     VentaComponent,
     InventarioComponent,
     BodegaComponent,
-    ReporteVentasComponent
+    ReporteVentasComponent,
+    DashboardComponent,
+    NavegacionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(routes),
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
